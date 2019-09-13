420b24403ec4c4c6e7eb07b8759890936e21452e
anand <anand901956@gmail.com>
2019-09-14 01:12:04 +0530

    hello world

---
+++ helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt"
+		"math"
+		"math/rand")
+
+func main(){
+	fmt.Println("Hello World!!")
+	fmt.Println("The Square root of 4 is -> ", math.Sqrt(4))
+	fmt.Println("random num between 1 to 100 is ->  ", rand.Intn(100))
+
+}
